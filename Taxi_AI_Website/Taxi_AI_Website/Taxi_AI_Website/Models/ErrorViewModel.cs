1e6c338b52c9805ca68dedc94795fd1a988c23be
Devin Radzwion <devinradzwion@gmail.com>
2021-02-15 20:17:41 -0500

    Web

---
+++ Taxi_AI_Website/Taxi_AI_Website/Taxi_AI_Website/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Taxi_AI_Website.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
