d00f66c37ced8802bee0208a1ad8c060711c6c6d
Devin Radzwion <devinradzwion@gmail.com>
2021-03-01 20:10:39 -0500

    Created Datamodel Library

---
+++ Taxi_AI_API/TaxiAI_Object_Lib/TaxiAI_REST.vb
@@ -0,0 +1,27 @@
+﻿Public Class TaxiAI_REST
+
+
+    Public Function GetCarFromID(ID As String) As Vehicle
+
+    End Function
+
+
+    Public Function RESTGet(URL) As String
+
+    End Function
+
+    Class Vehicle
+        Sub New()
+
+        End Sub
+    End Class
+
+    Class Location
+
+    End Class
+
+    Class Part
+
+    End Class
+
+End Class
